4bcbc64d8c61b9a681705c812349eb75e1dc16fe
JoDe <jodejpg@gmail.com>
2025-12-06 17:30:39 +0100

    feat: integrate event display with Swiper and add EventsContainer component

--- app/(Frontpage)/page.tsx
+++ app/(Frontpage)/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import AudioPlayer from "@/components/home/audio-player/AudioPlayer";
 import Welcome from "@/components/home/welcome-section/Welcome";
+import EventsContainer from "@/components/home/Events-container/EventsContainer";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <main className="w-full max-w-[1440px] flex-col items-center justify-betweensm:items-start">
         <Welcome />
+        <EventsContainer />
         <AudioPlayer />
       </main>
     </div>
---
+++ components/home/events-of-month/Events.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import Image from "next/image";
+import { Suspense } from "react";
+import EventCard from "../event-card/Event-card";
+
+interface Event {
+  id: number;
+  date: string;
+  title: string;
+  description: string;
+  location: string;
+  asset: {
+    url: string;
+  };
+}
+
+interface EventsProps {
+  events: Event[];
+}
+
+const Events = ({ events }: EventsProps) => {
+  return (
+    <Suspense
+      fallback={
+        <div className="mx-auto w-24 h-24 flex items-center justify-center my-24">
+          <Image
+            src="/assets/loader/madbars.gif"
+            alt="Loading..."
+            width={100}
+            height={100}
+            unoptimized
+          />
+        </div>
+      }
+    >
+      <section className="px-4 max-w-[708px] lg:max-w-full mx-auto">
+        <Swiper
+          modules={[Pagination]}
+          spaceBetween={24}
+          slidesPerView={1}
+          slidesPerGroup={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          pagination={{ clickable: true, type: "bullets" }}
+          breakpoints={{
+            1024: {
+              slidesPerView: 2,
+              slidesPerGroup: 2,
+            },
+          }}
+        >
+          {events?.map((event) => (
+            <SwiperSlide key={event.id} className="pb-16">
+              <EventCard event={event} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
+    </Suspense>
+  );
+};
+
+export default Events;
