3003ec967a1a7d3971d5e681012f260f2b1557dd
maen0002 <maen0002@stud.kea.dk>
2025-12-13 14:32:27 +0100

    start2

--- components/blog/comment-form/CommentForm.tsx
+++ components/blog/comment-form/CommentForm.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { useActionState, useEffect } from "react";
-import { submitComment } from "@/app/action/action";
+import { submitForm } from "@/app/action/action";
 import Button from "@/components/shared/button/Button";
 
-const CommentForm = ({ id, onSuccess }) => {
-  const [state, postComment] = useActionState(submitComment, { error: {} });
+interface CommentFormProps {
+  id: number;
+  onSuccess?: () => void;
+}
+
+const CommentForm = ({ id, onSuccess }: CommentFormProps) => {
+  const [state, postComment] = useActionState(submitForm, { error: {} });
   useEffect(() => {
     if (state?.success && onSuccess) {
       onSuccess();
@@ -14,13 +19,18 @@ const CommentForm = ({ id, onSuccess }) => {
   });
   console.log("State", state);
   return (
-    <form action={postComment} className="grid grid-cols-2 gap-4">
-      <input type="hidden" name="blogId" value={id}></input>
-      <input type="text" name="yourName" placeholder="Your Name" className="border px-3 py-6"></input>
-      <input type="email" name="yourEmail" placeholder="Your Email" className="border px-3 py-6"></input>
-      <textarea name="yourComment" rows={10} placeholder="Your Comment" className="border px-3 py-5 col-span-2"></textarea>
-      <Button isLink={false} text="Submit" stylePlace="place-self-end col-start-2"></Button>
-    </form>
+    <section>
+      {state.success && <p className="pb-6 text-primary">Comment {state.tableId} submitted successfully!</p>}
+      {state.success === false && <p className="pb-6">Something went wrong!</p>}
+
+      <form action={postComment} className="grid grid-cols-2 gap-4">
+        <input type="hidden" name="blogId" value={id}></input>
+        <input type="text" name="yourName" placeholder="Your Name" className="border px-3 py-6"></input>
+        <input type="email" name="yourEmail" placeholder="Your Email" className="border px-3 py-6"></input>
+        <textarea name="yourComment" rows={10} placeholder="Your Comment" className="border px-3 py-5 col-span-2"></textarea>
+        <Button isLink={false} text="Submit" stylePlace="place-self-end col-start-2"></Button>
+      </form>
+    </section>
   );
 };
 
