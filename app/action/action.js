8b5152d8d98935d52a6d8480bc503d8ab5477b00
maen0002 <maen0002@stud.kea.dk>
2025-12-13 13:33:14 +0100

    feat: enhance booking functionality with form validation and table selection

--- app/action/action.js
+++ app/action/action.js
@@ -1,49 +1,114 @@
 "use server";
 
-export const submitComment = async (prevState, formData) => {
+export const submitForm = async (prevState, formData) => {
   const error = {};
   let success = null;
   const yourName = formData.get("yourName");
   const yourEmail = formData.get("yourEmail");
   const yourComment = formData.get("yourComment");
   const blogID = formData.get("blogId");
+  const tableId = formData.get("tableId");
+  const yourDate = formData.get("yourDate");
+  const numberGuests = formData.get("numberGuests");
+  const yourTel = formData.get("yourTel");
 
   const currentTime = new Date();
 
-  if (!yourName) {
-    error.yourName = "this field is required";
+  const isBooking = tableId !== null && (numberGuests !== null || yourDate !== null || yourTel !== null);
+
+  const fetchType = isBooking ? "reservations" : "comments";
+
+  if (fetchType === "comments") {
+    if (!yourName) {
+      error.yourName = "this field is required";
+    }
+
+    if (!yourEmail) {
+      error.yourEmail = "this field is required";
+    }
+
+    if (!yourComment) {
+      error.yourComment = "this field is required";
+    } else if (yourComment.length < 2) {
+      error.yourComment = "Your comment has to be longer than 2 letters";
+    } else if (yourComment.length > 500) {
+      error.yourComment = "Your comment can't be longer than 500 letters";
+    }
+
+    if (Object.keys(error).length > 0) {
+      return { error, yourName, yourEmail, yourComment };
+    }
+  } else if (fetchType === "reservations") {
+    const compareDate = await fetch(`http://localhost:4000/reservations?date=${yourDate}&table=${tableId}`);
+    const dateSame = await compareDate.json();
+
+    if (dateSame.length > 0) {
+      error.yourDate = "This table is already booked for the selected date.";
+    }
+
+    if (!yourName) {
+      error.yourName = "this field is required";
+    }
+
+    if (!yourEmail) {
+      error.yourEmail = "this field is required";
+    }
+
+    if (!tableId) {
+      error.tableId = "You have to select a table";
+    }
+    if (!numberGuests || numberGuests <= 0) {
+      error.numberGuests = "Please enter a valid number of guests";
+    }
+    if (!yourDate) {
+      error.yourDate = "Please select a date";
+    } else if (new Date(yourDate) < currentTime) {
+      error.yourDate = "Please select a future date";
+    }
+
+    if (!yourTel) {
+      error.yourTel = "this field is required";
+    }
+    if (Object.keys(error).length > 0) {
+      return { error, yourName, yourEmail, tableId, yourDate, numberGuests, yourTel };
+    }
   }
 
-  if (!yourEmail) {
-    error.productPrice = "this field is required";
-  }
-
-  if (!yourComment) {
-    error.yourComment = "this field is required";
-  } else if (yourComment.length < 2) {
-    error.yourComment = "Your comment has to be longer than 2 letters";
-  }
-
-  if (Object.keys(error).length > 0) {
-    return { error, yourName, yourEmail, yourComment };
-  }
-
-  const response = await fetch("http://localhost:4000/comments", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      blogpostId: Number(blogID),
-      name: yourName,
-      content: yourComment,
-      date: currentTime,
-    }),
-  });
+  const response = await fetch(
+    `http://localhost:4000/${fetchType}`,
+    fetchType === "comments"
+      ? {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            blogpostId: Number(blogID),
+            name: yourName,
+            content: yourComment,
+            date: currentTime,
+          }),
+        }
+      : {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: yourName,
+            email: yourEmail,
+            table: tableId,
+            guests: numberGuests,
+            date: yourDate,
+            phone: yourTel,
+            comment: yourComment,
+          }),
+        }
+  );
 
   console.log(response);
 
   success = response.ok;
 
-  return { success, yourName, yourEmail, yourComment, currentTime };
+  return { success, yourName, yourEmail, yourComment, currentTime, tableId, yourDate, numberGuests, yourTel };
 };
--- components/booking/booking-form/BookingForm.tsx
+++ components/booking/booking-form/BookingForm.tsx
@@ -1,34 +1,53 @@
 "use client";
 
 import { useActionState, useEffect } from "react";
-import { submitComment } from "@/app/action/action";
+import { submitForm } from "@/app/action/action";
 import Button from "@/components/shared/button/Button";
 
 interface BookingFormProps {
   tableId?: number;
-  onSuccess?: () => void;
 }
 
-const BookingForm = ({ tableId, onSuccess }: BookingFormProps) => {
-  //   const [state, postComment] = useActionState(submitComment, { error: {} });
-  //   useEffect(() => {
-  //     if (state?.success && onSuccess) {
-  //       onSuccess();
-  //     }
-  //     [state?.success, onSuccess];
-  //   });
-  //   console.log("State", state);
+const BookingForm = ({ tableId }: BookingFormProps) => {
+  const [state, postReservation] = useActionState(submitForm, { error: {} });
+  console.log("State", state);
   return (
-    <form className="grid grid-cols-2 gap-4">
-      <input type="text" name="yourName" placeholder="Your Name" className="border px-3 py-6"></input>
-      <input type="email" name="yourEmail" placeholder="Your Email" className="border px-3 py-6"></input>
-      <input type="text" name="tableId" value={tableId} placeholder="Table ID" className="border px-3 py-6" readOnly></input>
-      <input type="number" name="numberGuests" placeholder="Number of Guests" className="border px-3 py-6"></input>
-      <input type="date" name="yourDate" placeholder="Select Date" className="border px-3 py-6"></input>
-      <input type="tel" name="yourTel" placeholder="Your Telephone" className="border px-3 py-6"></input>
-      <textarea name="yourComment" rows={10} placeholder="Your Comment" className="border px-3 py-5 col-span-2"></textarea>
-      <Button isLink={false} text="Submit" stylePlace="place-self-end col-start-2"></Button>
-    </form>
+    <section>
+      {state.success && <p className="pb-6 text-primary">Table {state.tableId} booked successfully!</p>}
+      {state.success === false && <p className="pb-6">Something went wrong!</p>}
+
+      <form action={postReservation} className="grid grid-cols-2 gap-4">
+        <div>
+          {state.error?.yourName && <p className="text-red-500">{state.error.yourName}</p>}
+          <input type="text" name="yourName" placeholder="Your Name" defaultValue={!state.success ? state.yourName : ""} className="border px-3 py-6 w-full"></input>
+        </div>
+        <div>
+          {state.error?.yourEmail && <p className="text-red-500">{state.error.yourEmail}</p>}
+          <input type="email" name="yourEmail" placeholder="Your Email" defaultValue={!state.success ? state.yourEmail : ""} className="border px-3 py-6 w-full"></input>
+        </div>
+        <div>
+          {state.error?.tableId && <p className="text-red-500">{state.error.tableId}</p>}
+          <input type="text" id="tableId" name="tableId" value={tableId ?? state.tableId ?? ""} defaultValue={!state.success ? state.tableId : ""} placeholder="Table ID" className="border px-3 py-6 w-full" readOnly></input>
+        </div>
+        <div>
+          {state.error?.numberGuests && <p className="text-red-500">{state.error.numberGuests}</p>}
+          <input type="number" name="numberGuests" placeholder="Number of Guests" defaultValue={!state.success ? state.numberGuests : ""} className="border px-3 py-6 w-full"></input>
+        </div>
+        <div>
+          {state.error?.yourDate && <p className="text-red-500">{state.error.yourDate}</p>}
+          <input type="date" name="yourDate" placeholder="Select Date" defaultValue={!state.success ? state.yourDate : ""} className="border px-3 py-6 w-full"></input>
+        </div>
+        <div>
+          {state.error?.yourTel && <p className="text-red-500">{state.error.yourTel}</p>}
+          <input type="tel" name="yourTel" placeholder="Your Telephone" defaultValue={!state.success ? state.yourTel : ""} className="border px-3 py-6 w-full"></input>
+        </div>
+        <div className="col-span-2">
+          {state.error?.yourComment && <p className="text-red-500">{state.error.yourComment}</p>}
+          <textarea name="yourComment" rows={10} placeholder="Your Comment" defaultValue={!state.success ? state.yourComment : ""} className="border px-3 py-5 w-full"></textarea>
+        </div>
+        <Button isLink={false} text="Submit" stylePlace="place-self-end col-start-2"></Button>
+      </form>
+    </section>
   );
 };
 
