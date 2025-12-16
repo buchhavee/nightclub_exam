025be72037f0b7858c8eceaa92ac63bb12a5e958
maen0002 <maen0002@stud.kea.dk>
2025-12-16 13:44:01 +0100

    fixed double fetch in error handling

--- app/(Other)/blog/page.tsx
+++ app/(Other)/blog/page.tsx
@@ -44,26 +44,21 @@ const FetchBlogs = async () => {
     if (!blogs || blogs.length === 0) {
       throw new Error("No blog posts available.");
     }
+    return blogs.map((blog: { title: string; id: string; author: string; asset: { url: string }; content: string }) => (
+      <section className="lg:grid grid-cols-[10%_minmax(0,_1fr)_minmax(0,_1fr)_10%]" key={blog.id}>
+        <Image className="col-span-2" src={blog.asset.url} alt={blog.title} width={600} height={600} style={imageStyle} unoptimized></Image>
+        <article className={`mx-8 my-10 flex flex-col ${Number(blog.id) % 2 == 0 ? "order-first col-start-2" : ""}`}>
+          <h3 className="mb-1 text-md">{blog.title}</h3>
+          <div className="flex gap-2 text-primary pb-4">
+            <p>BY: {blog.author}</p> <p>/</p> <FetchComments id={blog.id} /> <p>/</p> <p>16 Nov 2018</p>
+          </div>
+          <p className="line-clamp-3">{blog.content}</p>
+          <Button text="Read More" stylePlace="place-self-end mt-5" isLink={true} route={`/blog/${blog.id}`}></Button>
+        </article>
+      </section>
+    ));
   } catch (error) {
     console.error("Error fetching blog posts:", error);
     return <p className="text-center my-10">Unable to load blog posts at this time. Please try again later.</p>;
   }
-
-  const response = await fetch(`http://localhost:4000/blogposts`);
-  const blogs = await response.json();
-
-  console.log(blogs);
-  return blogs.map((blog: { title: string; id: string; author: string; asset: { url: string }; content: string }) => (
-    <section className="lg:grid grid-cols-[10%_minmax(0,_1fr)_minmax(0,_1fr)_10%]" key={blog.id}>
-      <Image className="col-span-2" src={blog.asset.url} alt={blog.title} width={600} height={600} style={imageStyle} unoptimized></Image>
-      <article className={`mx-8 my-10 flex flex-col ${Number(blog.id) % 2 == 0 ? "order-first col-start-2" : ""}`}>
-        <h3 className="mb-1 text-md">{blog.title}</h3>
-        <div className="flex gap-2 text-primary pb-4">
-          <p>BY: {blog.author}</p> <p>/</p> <FetchComments id={blog.id} /> <p>/</p> <p>16 Nov 2018</p>
-        </div>
-        <p className="line-clamp-3">{blog.content}</p>
-        <Button text="Read More" stylePlace="place-self-end mt-5" isLink={true} route={`/blog/${blog.id}`}></Button>
-      </article>
-    </section>
-  ));
 };
