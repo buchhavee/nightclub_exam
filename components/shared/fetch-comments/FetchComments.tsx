28e68297e3c1ea1d8584259ce77151f1f8a1f23a
JoDe <jodejpg@gmail.com>
2025-12-18 15:13:36 +0100

    Refactor FetchComments component to use SWR for data fetching and improve loading/error handling

--- components/shared/fetch-comments/FetchComments.tsx
+++ components/shared/fetch-comments/FetchComments.tsx
@@ -1,9 +1,19 @@
-const FetchComments = async ({ id }: { id: string }) => {
-  console.log("id", id);
-  const response = await fetch(`http://localhost:4000/blogposts/${id}?embed=comments`);
-  const data = await response.json();
-  console.log(data);
-  const commentsAmount = data.comments.length;
+"use client";
+
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const FetchComments = ({ id }: { id: string }) => {
+  const { data, error, isLoading } = useSWR(
+    `http://localhost:4000/blogposts/${id}?embed=comments`,
+    fetcher
+  );
+
+  if (isLoading) return <p>...</p>;
+  if (error) return <p>0 Comments</p>;
+
+  const commentsAmount = data?.comments?.length || 0;
   return <p>{commentsAmount} Comments</p>;
 };
 export default FetchComments;
