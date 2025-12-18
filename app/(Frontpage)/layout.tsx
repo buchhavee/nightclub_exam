585d652faa0b1bf90ecbedfe5206a6585bd7073d
JoDe <jodejpg@gmail.com>
2025-12-18 15:21:11 +0100

    Add overflow-x-hidden class to body in layout components

--- app/(Frontpage)/layout.tsx
+++ app/(Frontpage)/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={ubuntu.variable}>
-      <body className="font-ubuntu">
+      <body className="font-ubuntu overflow-x-hidden">
         <Hero />
         <Navigation />
         {children}
